f98812790469bd2d4507c86cc915d8beda80de21
ArtemiuszAnton <elvispresley9122@gmail.com>
2024-05-10 21:52:31 +0200

    add styles for choose vacancy

--- client/src/Pages/Vacancy/Vacancy.jsx
+++ client/src/Pages/Vacancy/Vacancy.jsx
@@ -7,19 +7,25 @@ function Vacancy() {
     { name: 'Работник декорации, дизайнер (ТЦ Амбар)', salary: 'з/п 29000 rub', workday: 'Сменный график работы', city: 'Самара' },
     { name: 'Менеджер-дизайнер', salary: 'з/п 55000 - 95000 rub', workday: 'Полный рабочий день', city: 'Тюмень' }]
 
-    return <div className={style.vacancies}>
+    return <div className={style.wrapper}>
 
-        <div className={style.mui}>
+        <div className={style.form}>
             <TextField id="outlined-basic" label="Введите название вакансии" variant="outlined" />
             <Button variant="contained">Поиск</Button>
         </div>
 
         {arr.map((el, i) =>
-            <div key={i}>
+            <div className={style.item} key={i}>
                 <h2>{el.name}</h2>
-                <p>{el.salary}</p>
-                <p>{el.workday}</p>
-                <p>{el.city}</p>
+                <div className={style.schedule}>
+                    <h3>{el.salary}</h3>
+                    <h4>•</h4>
+                    <p>{el.workday}</p>
+                </div>
+                <div className={style.location}>
+                    <div className={style.geo}></div>
+                    <p>{el.city}</p>
+                </div>
             </div>
         )}
     </div>
